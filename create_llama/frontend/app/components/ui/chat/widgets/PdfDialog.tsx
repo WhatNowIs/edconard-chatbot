79b3d8188551fc1b0b922bf91d7bd88abb14ce63
Byamasu Patrick <ptrckbyamasu@gmail.com>
2024-06-06 13:36:29 +0200

    Added highlight feature on PDF Viewer

--- create_llama/frontend/app/components/ui/chat/widgets/PdfDialog.tsx
+++ create_llama/frontend/app/components/ui/chat/widgets/PdfDialog.tsx
@@ -9,15 +9,78 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "../../drawer";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { OBSERVER_THRESHOLD_PERCENTAGE } from "./pdfDisplayConstants";
+import { useInView } from "react-intersection-observer";
+import { usePdfFocus } from "@/app/context/pdf";
+import debounce from "lodash.debounce";
 
 export interface PdfDialogProps {
   documentId: string;
   path: string;
   url: string;
   trigger: React.ReactNode;
+  scale: number;
+  pageNumber: number;
+  setPageInView: (n: number) => void;
 }
 
 export default function PdfDialog(props: PdfDialogProps) {
+  const { pdfFocusState } = usePdfFocus();
+  const [isHighlighted, setIsHighlighted] = useState(false);
+   // Get which page is in view from an intersection observer
+   const { ref: inViewRef, inView } = useInView({
+    threshold: OBSERVER_THRESHOLD_PERCENTAGE * Math.min(1 / props.scale, 1),
+  });
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Use `useCallback` so we don't recreate the function on each render
+  // Need to set two Refs, one for the intersection observer, one for the container
+  const setRefs = useCallback(
+    (node: HTMLDivElement | null | undefined) => {
+      // Ref's from useRef needs to have the node assigned to `current`
+      (containerRef as React.MutableRefObject<HTMLDivElement | null>).current =
+        node as HTMLDivElement | null;
+
+      // Callback refs, like the one from `useInView`, is a function that takes the node as an argument
+      inViewRef(node);
+    },
+    [inViewRef]
+  );
+
+  useEffect(() => {
+    if (inView) {
+      props.setPageInView(props.pageNumber);
+    }
+  }, [inView, props.pageNumber, props.setPageInView, inViewRef]);
+
+
+  const documentFocused = pdfFocusState.documentId === props.documentId;
+
+  useEffect(() => {
+    maybeHighlight();
+  }, [documentFocused, inView]);
+
+  const maybeHighlight = useCallback(
+    debounce(() => {
+      if (
+        documentFocused &&
+        pdfFocusState.citation?.pageNumber === props.pageNumber + 1 &&
+        !isHighlighted
+      ) {
+        multiHighlight(
+          pdfFocusState.citation?.snippet,
+          props.pageNumber,
+          pdfFocusState.citation?.color
+        );
+        setIsHighlighted(true);
+      }
+    }, 50),
+    [pdfFocusState.citation?.snippet, props.pageNumber, isHighlighted]
+  );
+
+
   return (
     <Drawer direction="left">
       <DrawerTrigger>{props.trigger}</DrawerTrigger>
@@ -40,7 +103,7 @@ export default function PdfDialog(props: PdfDialogProps) {
             <Button variant="outline">Close</Button>
           </DrawerClose>
         </DrawerHeader>
-        <div className="m-4">
+        <div className="m-4"  ref={setRefs}>
           <PdfFocusProvider>
             <PDFViewer
               file={{
@@ -54,3 +117,7 @@ export default function PdfDialog(props: PdfDialogProps) {
     </Drawer>
   );
 }
+function multiHighlight(snippet: any, pageNumber: any, color: any) {
+  throw new Error("Function not implemented.");
+}
+
