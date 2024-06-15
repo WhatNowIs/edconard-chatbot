b2a2d1a9e03a99ac6d2243cecf138d3a01622d2d
Byamasu Patrick <ptrckbyamasu@gmail.com>
2024-06-16 00:01:26 +0200

    Added account settings section

--- create_llama/frontend/app/components/left-nav.tsx
+++ create_llama/frontend/app/components/left-nav.tsx
@@ -1,24 +1,27 @@
+"use client"
+
 import Image from "next/image";
-import { Button } from "./ui/button";
-import { EdConardLogo, Harmburger, PlusIcon, ShevronDown } from "./ui/chat/icons/main-icons";
-import RecentThreads from "./ui/chat/recent-thread";
-import Workspaces from "./ui/chat/workspace";
-import ChatBundles from "./ui/chat/chat-bundles";
-import Marketplace from "./ui/chat/marketplace";
+import { usePathname } from 'next/navigation';
+import { Button } from "@/app/components/ui/button";
+import { EdConardLogo, Harmburger, PlusIcon } from "./ui/chat/icons/main-icons";
+import RecentThreads from "@/app/components/ui/chat/recent-thread";
+import Workspaces from "@/app/components/ui/chat/workspace";
+import ChatBundles from "@/app/components/ui/chat/chat-bundles";
+import Marketplace from "@/app/components/ui/chat/marketplace";
+import Link from "next/link";
 
 
 
 export default function LeftNav() {
-
-
+    const currentPath = usePathname();
 
     return (
         <div className="w-64 flex flex-col h-screen bg-white border-r border-gray-200 p-4">
             <div className="w-full flex mb-4 justify-between items-center gap-2">
-                <div className="flex gap-2">
+                <Link href={"/"} className="flex gap-2">
                     <EdConardLogo />
                     <span className="text-2xl">CRI</span>
-                </div>
+                </Link>
                 <Harmburger />
             </div>
             <div className="flex items-center mb-6 border p-2 rounded-md">
@@ -33,16 +36,21 @@ export default function LeftNav() {
                 <div className="text-sm text-gray-500">Marc Hill</div>
                 <div className="text-sm text-gray-500 text-xs">mhill@edconard.com</div>
                 </div>
-            </div>
-            
-            <Button type="submit" className="w-full flex items-center mb-6">
-                <PlusIcon />
-                New Thread
-            </Button>
-            <Workspaces />
-            <RecentThreads />
-            <ChatBundles />
-            <Marketplace />
+            </div>            
+            {
+                currentPath !== "/accounts" && (
+                    <>
+                        <Button type="submit" className="w-full flex items-center mb-6">
+                            <PlusIcon />
+                            New Thread
+                        </Button>
+                        <Workspaces />
+                        <RecentThreads />
+                        <ChatBundles />
+                        <Marketplace />
+                    </>
+                )
+            }
         </div>
     );
 }
