4e8ddaff04b52df6fd77d252733826e5b41110a9
Byamasu Patrick Paul <43136469+byamasu-patrick@users.noreply.github.com>
2024-08-16 19:05:43 +0200

    Added toast provider

--- create_llama/frontend/app/layout.tsx
+++ create_llama/frontend/app/layout.tsx
@@ -1,10 +1,11 @@
-import React from 'react'; 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import "./markdown.css";
+import React from "react";
+import { ToastProvider } from "./components/ui/toast";
 import { AuthProvider } from "./context/auth-context";
 import { ChatProvider } from "./context/chat-context";
+import "./globals.css";
+import "./markdown.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
   description: `
     AI-powered chatbot designed to answer questions and deliver insights regarding macroeconomics. 
     The chatbot will mimic the rhetorical and analytical style of Edward Conard,
-  `
+  `,
 };
 
 export default function RootLayout({
@@ -23,13 +24,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>   
-             
-        <AuthProvider>
-          <ChatProvider>
-            {children}
-          </ChatProvider>
-        </AuthProvider>
+      <body className={inter.className}>
+        <ToastProvider>
+          <AuthProvider>
+            <ChatProvider>{children}</ChatProvider>
+          </AuthProvider>
+        </ToastProvider>
       </body>
     </html>
   );
