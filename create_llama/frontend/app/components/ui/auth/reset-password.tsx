b8093a7887cb385c63f405d25b4a4e6155d7bb6d
Byamasu Patrick Paul <43136469+byamasu-patrick@users.noreply.github.com>
2024-08-16 19:03:48 +0200

    Added error handling for resetting password

--- create_llama/frontend/app/components/ui/auth/reset-password.tsx
+++ create_llama/frontend/app/components/ui/auth/reset-password.tsx
@@ -1,84 +1,116 @@
-"use client"
+"use client";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'; 
-import { useSearchParams } from 'next/navigation';
+import {
+  ResetPasswordFormSchema,
+  resetPassword,
+} from "@/app/service/user-service";
+import base64ToString from "@/app/utils/shared";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
 import { Button } from "../button";
-import { useRouter } from 'next/navigation';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { ResetPasswordFormSchema, resetPassword } from '@/app/service/user-service';
 import { Form, FormControl, FormField, FormItem } from "../form";
 import { Input } from "../input";
-import base64ToString from '@/app/utils/shared';
-import Link from 'next/link';
+import { cn } from "../lib/utils";
+import { useToast } from "../use-toast";
 
-export default function ResetPasswordFrom(){
-    const form = useForm({
-        resolver: zodResolver(ResetPasswordFormSchema)
+export default function ResetPasswordFrom() {
+  const form = useForm({
+    resolver: zodResolver(ResetPasswordFormSchema),
+  });
+  const { toast } = useToast();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const uid = searchParams.get("uid");
+  const otp = searchParams.get("otp");
+  const verification_type = searchParams.get("verification_type");
+
+  const reset = async (
+    code: string,
+    email: string,
+    verification_type: string,
+    password: string,
+  ) => {
+    const response = await resetPassword({
+      code,
+      otp_type: base64ToString(verification_type),
+      email: base64ToString(email),
+      password,
     });
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const uid = searchParams.get('uid');
-    const otp = searchParams.get('otp');
-    const verification_type = searchParams.get('verification_type');
 
-    const reset = async (code: string, email: string, verification_type: string, password: string) => {        
-        const response = await resetPassword({
-            code,
-            otp_type: base64ToString(verification_type),
-            email: base64ToString(email),
-            password
-        });
-
-        if(response.message && response.status === 200){
-            router.push("/signin");
-        }
+    if (response.message && response.status === 200) {
+      router.push("/signin");
     }
 
-    const onSubmit = (data: any) => {
-        console.log(data);
-        const currentData = data as { new_password: string; confirm_password: string; };        
-        reset(otp as string, uid as string, verification_type as string, currentData.new_password).catch((error) => console.log(error));
-    }
+    toast({
+      className: cn(
+        "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4 text-red-500",
+      ),
+      title: "Failed to update password",
+      description: response.message,
+    });
+  };
 
-    return (   
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mb-4">     
-                <FormField
-                    control={form.control}
-                    name="new_password"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormControl>
-                        <Input type="password" {...field} placeholder="********" />
-                        </FormControl>
-                    </FormItem>
-                    )}
-                />
-                 <FormField
-                    control={form.control}
-                    name="confirm_password"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormControl>
-                        <Input type="password" {...field} placeholder="********" />
-                        </FormControl>
-                    </FormItem>
-                    )}
-                />
-                <div className="space-y-6">
-                    <Button onClick={onSubmit} type="submit" className="w-full flex items-center mb-6">
-                        Submit
-                    </Button>
-                </div>
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Want to sign in to your account?{' '}
-                    <Link href="/signin" className="font-semibold leading-6 text-secondary-foreground">
-                        Log in
-                    </Link>
-                </p>
-            </form>
-        </Form>
-    )
+  const onSubmit = (data: any) => {
+    console.log(data);
+    const currentData = data as {
+      new_password: string;
+      confirm_password: string;
+    };
+    reset(
+      otp as string,
+      uid as string,
+      verification_type as string,
+      currentData.new_password,
+    ).catch((error) => console.log(error));
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mb-4">
+        <FormField
+          control={form.control}
+          name="new_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" {...field} placeholder="********" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" {...field} placeholder="********" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className="space-y-6">
+          <Button
+            onClick={onSubmit}
+            type="submit"
+            className="w-full flex items-center mb-6"
+          >
+            Submit
+          </Button>
+        </div>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Want to sign in to your account?{" "}
+          <Link
+            href="/signin"
+            className="font-semibold leading-6 text-secondary-foreground"
+          >
+            Log in
+          </Link>
+        </p>
+      </form>
+    </Form>
+  );
 }
--- create_llama/frontend/app/components/ui/auth/signup-form.tsx
+++ create_llama/frontend/app/components/ui/auth/signup-form.tsx
@@ -1,113 +1,120 @@
-"use client"
+"use client";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'; 
-import { useState } from "react";
-import { useRouter } from 'next/navigation';
-import { UserFormType, UserSchema, createUserAccount } from "@/app/service/user-service";
-import { useForm } from 'react-hook-form';
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../form";
+import {
+  UserFormType,
+  UserSchema,
+  createUserAccount,
+} from "@/app/service/user-service";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "../input";
-import { useToast } from "../use-toast";
-import { cn } from "../lib/utils";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { SubmitButton } from "../custom/submitButton";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../form";
+import { Input } from "../input";
+import { cn } from "../lib/utils";
+import { useToast } from "../use-toast";
 
-export default function SignupForm(){
-    const form = useForm({
-        resolver: zodResolver(UserSchema),
-    })
-    const { toast } = useToast();
-    const router = useRouter();
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+export default function SignupForm() {
+  const form = useForm({
+    resolver: zodResolver(UserSchema),
+  });
+  const { toast } = useToast();
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-    const onSubmit = async (data: any) => {        
-        setIsSubmitting(true);
-        // Send the data to the server
-        try {
-            const configData = await createUserAccount(data as UserFormType);
-            console.log(configData);
+  const onSubmit = async (data: any) => {
+    setIsSubmitting(true);
+    // Send the data to the server
+    try {
+      const configData = await createUserAccount(data as UserFormType);
 
-            router.push("/auth/verify-account-msg");
-        } catch (err) {
-            console.error(err);
-            toast({
-                className: cn(
-                "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4 text-red-500",
-                ),
-                title: "Failed to update config",
-            });
-        }
-        setIsSubmitting(false);
+      router.push("/auth/verify-account-msg");
+    } catch (err) {
+      console.error(err);
+      toast({
+        className: cn(
+          "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4 text-red-500",
+        ),
+        title: "Failed to update config",
+        description: "An error has occurred while creating your account",
+      });
     }
+    setIsSubmitting(false);
+  };
 
-    return (        
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mb-4">                 
-                <div className="flex gap-4">
-                    <FormField
-                        control={form.control}
-                        name="first_name"
-                        render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>First name</FormLabel>
-                            <FormControl>
-                            <Input {...field} />
-                            </FormControl>
-                        </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="last_name"
-                        render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Last name</FormLabel>
-                            <FormControl>
-                            <Input {...field} />
-                            </FormControl>
-                        </FormItem>
-                        )}
-                    />   
-                </div>                      
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Email address</FormLabel>
-                        <FormControl>
-                        <Input {...field} />
-                        </FormControl>
-                    </FormItem>
-                    )}
-                />             
-                <FormField
-                    control={form.control}
-                    name="phone_number"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Phone number</FormLabel>
-                        <FormControl>
-                        <Input {...field} />
-                        </FormControl>
-                    </FormItem>
-                    )}
-                />                
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                        <Input type="password" {...field} />
-                        </FormControl>
-                    </FormItem>
-                    )}
-                />             
-                <SubmitButton isSubmitting={isSubmitting} text="Create" className="w-full flex items-center" />           
-            </form>
-        </Form>
-    )
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mb-4">
+        <div className="flex gap-4">
+          <FormField
+            control={form.control}
+            name="first_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email address</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone_number"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone number</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <SubmitButton
+          isSubmitting={isSubmitting}
+          text="Create"
+          className="w-full flex items-center"
+        />
+      </form>
+    </Form>
+  );
 }
